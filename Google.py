6db5d8fc56599997a22dd18d0b4503a91f351169
Legedith <jatindehmiwal@gmail.com>
2019-08-30 18:47:35 +0530

    Initial Commit

---
+++ Google.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup
+with open("Qwiklabs.html", encoding='utf-8') as fp:
+    soup = BeautifulSoup(fp,'html.parser')
+    a = soup.find_all('code',attrs={'class': None})
+    f = open("code.txt","w")
+    for i in a:
+        s=i.get_text()
+        f.write(s)
+        print(s)
+    f.close()
